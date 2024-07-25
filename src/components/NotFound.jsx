5854e5acbb613399ba8ecff87d50edb7f9b301ba
Sidik Komarudiansah <sidikkomarudiansah@Sidiks-MacBook-Pro.local>
2024-07-25 20:57:14 +0700

    add component notfound and unauthorized

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-	return (
-		<div>
-			<h1>Not Found</h1>
-		</div>
-	)
-}
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+      <h1 className="text-4xl font-bold text-yellow-600">404</h1>
+      <p className="mt-4 text-lg">
+        Halaman yang anda cari tidak ada.
+      </p>
+      <button
+        onClick={handleGoHome}
+        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition-colors duration-300"
+      >
+        Kembali ke Dashboard
+      </button>
+    </div>
+  );
+};
 
 export default NotFound;
--- src/components/UnAuthorization.jsx
+++ src/components/UnAuthorization.jsx
@@ -1,9 +1,24 @@
+import { useNavigate } from "react-router-dom";
 
+const UnAuthorized = () => {
+  const navigate = useNavigate();
 
-const UnAuthorization = () => {
-	return (
-		<div>UnAuthorization</div>
-	)
-}
+  const handleGoHome = () => {
+    navigate("/");
+  };
 
-export default UnAuthorization;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+      <h1 className="text-4xl font-bold text-yellow-600">403</h1>
+      <p className="mt-4 text-lg">Anda tidak diizinkan mengakses halaman ini!</p>
+      <button
+        onClick={handleGoHome}
+        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition-colors duration-300"
+      >
+        Kembali ke Dashboard
+      </button>
+    </div>
+  );
+};
+
+export default UnAuthorized;
